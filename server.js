67d064938648a501a4ffc128689c0f2d04b7afe7
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2024-03-08 23:27:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const port = fs.readFileSync('./config/port.conf','utf8').trim();
+const port = process.env.PORT || fs.readFileSync('./config/port.conf','utf8').trim();
 const server = express();
 
 server.use(express.static(path.resolve(__dirname,'public')));

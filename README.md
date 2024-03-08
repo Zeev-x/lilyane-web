741da2c998dbf3544de6923170981d9598fe55a6
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2024-03-08 23:13:29 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Source kode ini 100% di buat oleh lilyane.
+
+## Website saat ini masih dalam tahap pengembangan.
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const port = fs.readFileSync('./config/port.conf','utf8').trim();
+const server = express();
+
+server.use(express.static(path.resolve(__dirname,'public')));
+server.listen(port,() => {
+    console.log('Server runing on port',port);
+});
+
+//------------------------------------------//
+
+server.get('/home',(req,res) => {
+    var text = fs.readFileSync('./README.md','utf8');
+    res.send(text);
+});
+
+//------------------------------------------//
+server.use((req,res) => {
+    var path = req.path;
+    var errHtml = fs.readFileSync('./error/error.html','utf8');
+        errHtml = errHtml.replace('[PATH]',path);
+    res.send(errHtml);
+});
